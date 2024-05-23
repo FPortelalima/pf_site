4816952e1bb82c62c11b3eae566fc5201ae63089
FPortelalima <ich@ich>
2024-05-23 14:29:49 -0300

    Add Jenkinsfile

--- mysql/scripts/init.sql
+++ mysql/scripts/init.sql
@@ -5,3 +5,10 @@ CREATE TABLE produtos (
 
 INSERT INTO produtos (name) VALUES ('Iphone 14');
 INSERT INTO produtos (name) VALUES ('Playstation');
+INSERT INTO produtos (name) VALUES ('Xbox 360');
+INSERT INTO produtos (name) VALUES ('Samsung Galaxy');
+INSERT INTO produtos (name) VALUES ('Xiaomi');
+INSERT INTO produtos (name) VALUES ('Galaxy Tablet');
+INSERT INTO produtos (name) VALUES ('SSD Nvme Kingston');
+INSERT INTO produtos (name) VALUES ('Placa de Video Nvdia 3080 TI');
+INSERT INTO produtos (name) VALUES ('Notebook ROG');
