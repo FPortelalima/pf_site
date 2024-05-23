784494ff16d39ee446d07601cac7b685023f7f41
FPortelalima <102562803+FPortelalima@users.noreply.github.com>
2024-05-23 07:07:13 -0300

    Add files via upload

---
+++ init.sql
@@ -0,0 +1,7 @@
+CREATE TABLE produtos (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
+
+INSERT INTO produtos (name) VALUES ('Iphone 14');
+INSERT INTO produtos (name) VALUES ('Playstation');
